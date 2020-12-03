5ed57562fb45db79261991928825df94351feff2
Pedro Lustosa <p.h.lustosa@hotmail.com>
2020-12-03 14:31:10 -0300

    Update In all View New Btn(btn Top) New CSS/JS

--- QuicklyRecycle/Controllers/CollectorsController.cs
+++ QuicklyRecycle/Controllers/CollectorsController.cs
@@ -1,153 +1,156 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using QuicklyRecycle.Data;
 using QuicklyRecycle.Models;
+using X.PagedList;
 
 namespace QuicklyRecycle.Controllers
 {
-    public class CollectorsController : Controller
-    {
-        private readonly ApplicationDbContext _context;
+	public class CollectorsController : Controller
+	{
+		private readonly ApplicationDbContext _context;
 
-        public CollectorsController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+		public CollectorsController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
 
-        // GET: Collectors
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Collector.ToListAsync());
-        }
+		// GET: Collectors
+		public async Task<IActionResult> Index(int? pagina)
 
-        // GET: Collectors/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		{
 
-            var collector = await _context.Collector
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (collector == null)
-            {
-                return NotFound();
-            }
+			const int itensPorPagina = 10;
+			int numeroPagina = (pagina ?? 1);
 
-            return View(collector);
-        }
+			return View(await _context.Collector.ToPagedListAsync(numeroPagina, itensPorPagina));
+		}
 
-        // GET: Collectors/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+		// GET: Collectors/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-        // POST: Collectors/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Landline,CellPhone")] Collector collector)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(collector);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(collector);
-        }
+			var collector = await _context.Collector
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (collector == null)
+			{
+				return NotFound();
+			}
 
-        // GET: Collectors/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+			return View(collector);
+		}
 
-            var collector = await _context.Collector.FindAsync(id);
-            if (collector == null)
-            {
-                return NotFound();
-            }
-            return View(collector);
-        }
+		// GET: Collectors/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: Collectors/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Landline,CellPhone")] Collector collector)
-        {
-            if (id != collector.Id)
-            {
-                return NotFound();
-            }
+		// POST: Collectors/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to, for 
+		// more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,Name,Landline,CellPhone")] Collector collector)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(collector);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(collector);
+		}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(collector);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CollectorExists(collector.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(collector);
-        }
+		// GET: Collectors/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-        // GET: Collectors/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+			var collector = await _context.Collector.FindAsync(id);
+			if (collector == null)
+			{
+				return NotFound();
+			}
+			return View(collector);
+		}
 
-            var collector = await _context.Collector
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (collector == null)
-            {
-                return NotFound();
-            }
+		// POST: Collectors/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to, for 
+		// more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Landline,CellPhone")] Collector collector)
+		{
+			if (id != collector.Id)
+			{
+				return NotFound();
+			}
 
-            return View(collector);
-        }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(collector);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!CollectorExists(collector.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(collector);
+		}
 
-        // POST: Collectors/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var collector = await _context.Collector.FindAsync(id);
-            _context.Collector.Remove(collector);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+		// GET: Collectors/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-        private bool CollectorExists(int id)
-        {
-            return _context.Collector.Any(e => e.Id == id);
-        }
-    }
+			var collector = await _context.Collector
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (collector == null)
+			{
+				return NotFound();
+			}
+
+			return View(collector);
+		}
+
+		// POST: Collectors/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var collector = await _context.Collector.FindAsync(id);
+			_context.Collector.Remove(collector);
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool CollectorExists(int id)
+		{
+			return _context.Collector.Any(e => e.Id == id);
+		}
+	}
 }
